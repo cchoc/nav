8e2aea87faea525ba85b886c75ab150cfd2beca8
cchoc <2238992454@qq.com>
2025-08-26 09:29:37 +0000

    Deploying to gh-pages from @ cchoc/nav@2bce40351d7ac6923e346cab7126502ba881a804 🚀

---
+++ chunk-UF5DUM2E.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-GIPM5LVF.js";import{pb as r,yb as e}from"./chunk-F4RPIGNY.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
